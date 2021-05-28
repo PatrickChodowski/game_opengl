9c53b513992541b19cb53a158c4dd5d0596f3834
patrick <chodowski.patrick@gmail.com>
2021-05-28 15:37:36 +0200

    added hero struct

--- components/hero.h
+++ components/hero.h
@@ -1,11 +1,30 @@
 #ifndef HERO_H
 #define HERO_H
 
-
-
 namespace hero
 {
-  
+  struct Hero
+  {
+    // position
+    int x;
+    int y;
+
+    // display information
+    int current_movement_state;
+    
+    // personal information
+    std::string name;
+    int level;
+
+    // statistics
+    int exp;
+    int speed;
+    int hp;
+    int dmg;
+    int def;
+    int mobs_killed;
+
+  }; 
 
 
 }
