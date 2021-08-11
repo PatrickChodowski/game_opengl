e42984e20a40ee5028c5cb6c2118f0c38efec973
patrick <chodowski.patrick@chalhoub.com>
2021-08-12 00:31:30 +0400

    initialization collisions

---
+++ components/collisions.h
@@ -0,0 +1,28 @@
+
+#ifndef COLLISIONS_H
+#define COLLISIONS_H
+
+namespace colls
+{
+
+    void get_distance_between_points()
+    {
+
+    }
+
+    void get_distances()
+    {
+        
+    }
+
+    void find_broad_collisions()
+    {
+
+    }
+
+
+
+
+}
+
+#endif
