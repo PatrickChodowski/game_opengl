5a34d04c4ddcb8a79ab09572f02290b4a40d571d
patrick <chodowski.patrick@chalhoub.com>
2021-09-25 10:04:35 +0400

    adding animations methods and td improvement

--- opengl/anims.h
+++ opengl/anims.h
@@ -4,8 +4,22 @@
 namespace anims
 {
 
-    void read_anim_data()
+    void set_frame(int quad_index, int frame_id)
     {
+        
+    }
+
+    void play(int quad_index, int event_id)
+    {
+
+        // check if event_id exists in given texture animation catalog:
+        if(textures::Catalog[quads::AllQuads[quad_index].texture_id].anims.count(event_id) > 0)
+        {
+            float time_since_last_update = timer::get_elapsed_time(quads::AllQuads[quad_index].frame_update_time);
+
+
+        }
+
     }
 
 
