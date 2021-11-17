68640d08199034bdd9ab4459ccd43df5a52e01b9
patrick <chodowski.patrick@gmail.com>
2021-11-17 08:28:43 +0100

    map of functions for mouse click

--- modules/mouse.cpp
+++ modules/mouse.cpp
@@ -2,10 +2,15 @@
 #include "quads.h"
 
 #include <iostream>
+#include <map>
 #include <string>
+
+#include "../dictionary.h"
 
 namespace mouse2
 {
+
+  std::map <int , sig_ptr> click = {};
 
   void _find_clicked_quads(float click_x, float click_y)
   {
@@ -18,9 +23,45 @@ namespace mouse2
                   << " object ID: " << q.object_id 
                   << " object type id " << q.object_type_id 
                   << std::endl;
+
+        mouse2::click[q.object_type_id](q.object_id);
       }
     };
   }
+
+  // how to handle different types of click -> on menu, on map, on entity, left click, right click etc.
+
+
+  void _click_menu(int object_id)
+  {
+    std::cout << "Clicked on menu object id: " << object_id << std::endl;
+  };
+
+  void _click_entity(int object_id)
+  {
+    std::cout << "Clicked on entity object id: " << object_id << std::endl;
+  };
+
+  void _click_map(int object_id)
+  {
+    std::cout << "Clicked on map object id: " << object_id << std::endl;
+  };
+
+  void _click_text(int object_id)
+  {
+    std::cout << "Clicked on text object id: " << object_id << std::endl;
+  };
+
+
+  void init()
+  {
+    mouse2::click[OBJECT_TYPE_ENTITY] = _click_entity;
+    mouse2::click[OBJECT_TYPE_MAP] = _click_map;
+    mouse2::click[OBJECT_TYPE_MENU] = _click_menu;
+    mouse2::click[OBJECT_TYPE_TEXT] = _click_text;
+  }
+
+
 
   void print_mouse(SDL_MouseMotionEvent e, const char* name)
   {
