bd96f8081e30057dd93d2ee1abc28deaabe0d74b
patrick <chodowski.patrick@gmail.com>
2021-12-11 14:11:07 +0100

    pybind is the king

--- modules/py.cpp
+++ modules/py.cpp
@@ -1,18 +1,19 @@
 
 #include <iostream>
 
-#include <boost/python.hpp>
+#include "../dependencies/pybind11/pybind11.h"
 
-char const* greet()
+namespace py = pybind11;
+
+char const* greet2()
 { 
-    std::cout << "hello world" << std::endl;
-   return "hello, world";
+    std::cout << "hello world2" << std::endl;
+   return "hello, world2";
 };
 
-BOOST_PYTHON_MODULE(gamescripts)
-{
-    using namespace boost::python;
-    def("greet", greet);
+PYBIND11_MODULE(gamescripts, m) {
+    m.doc() = "pybind11 example plugin"; // optional module docstring
+    m.def("greet2", &greet2, "A function hello world");
 }
 
-int main() { greet(); return 0;};
+int main() { greet2(); return 0;};
