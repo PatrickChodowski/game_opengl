3a14910e812cb1e90d7b3e3fa600afc13f3e7d49
patrick <chodowski.patrick@gmail.com>
2021-12-13 09:35:02 +0100

    its some sort of black magic

--- scripts/py.cpp
+++ scripts/py.cpp
@@ -11,7 +11,7 @@ namespace py = pybind11;
 
 namespace scripts
 {
-  py::scoped_interpreter guard;
+  py::scoped_interpreter guard{};
 
   // this can be good:
   // py::exec("print 1");
@@ -22,7 +22,10 @@ namespace scripts
     // auto kwargs = py::dict("name"_a="World", "number"_a=42);
     // auto message = "Hello, {name}! The answer is {number}"_s.format(**kwargs);
 
-    auto locals = py::dict();
+    // when executing the script, access variables from CPP, copy to python, use this to run python logic, return some data, use this data
+
+    using namespace py::literals;
+    auto locals = py::dict("name"_a="World", "number"_a=42);
     py::eval_file(script_name, py::globals(), locals);
     auto test_variable = locals["test_variable"].cast<int>();
     std::cout << "test variable: " << test_variable << std::endl;
