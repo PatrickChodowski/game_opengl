e60d1d2b929baf8e998f15a90cf2fa02c08684ec
patrick <chodowski.patrick@gmail.com>
2021-12-17 08:08:57 +0100

    added python terminal

---
+++ scripts/app.py
@@ -0,0 +1,49 @@
+import os
+from game_api import list_funcs, parse_command, print_help
+
+# python3 ./Documents/projects/game_opengl/scripts/app.py
+# python3 ./scripts/app.py
+
+
+def display_title_bar():
+    # Clears the terminal screen, and displays a title bar.
+    os.system('clear')
+              
+    print("\t**********************************************")
+    print("\t***             Game Terminal              ***")
+    print("\t**********************************************")
+
+def get_user_command():
+    # Let users know what they can do.
+    print("\nPlease provide command. Arguments will be split by whitespaces\n")
+    print("[ls] List functions")
+    print("[q]  Quit")
+    
+    return input("Command: ")
+
+### MAIN PROGRAM ###
+
+# Set up a loop where users can choose what they'd like to do.
+command = ""
+
+display_title_bar()
+while command != 'q':
+
+    command = get_user_command()
+    display_title_bar()
+
+    if (command != 'q') & (command != 'ls'):
+      parse_command(command)
+
+    elif "?" in command:
+      print_help(command)
+
+    elif command == 'q':
+      print("\nGoodbye")
+
+    elif command == 'ls':
+      list_funcs()
+
+    else:
+      print("\nI didn't understand that choice.\n")
+
--- scripts/game_api.py
+++ scripts/game_api.py
@@ -1,6 +1,109 @@
-# function name, function id
 
-func_dict = {"_spawn_mob": 0,
-             "_drop_mob":  1,
-             "_interact_npc": 2
+funcs = {
+  "_spawn_mob": {"first": "spawn", 
+                 "second": "mob",
+                 "args": ["mob_type_id", "x", "y"],
+                 "id": 0},
+
+  "_drop_mob": {"first": "drop", 
+                 "second": "mob",
+                 "args": ["entity_id"],
+                 "id": 1},
+
+  "_interact_npc": {"first": "interact", 
+                    "second": "npc",
+                    "args": ["entity_id", "interaction_value"],
+                    "id": 2},
+
 }
+
+
+def _print_args(func_name: str) -> None:
+  print(f"  args: ")
+  for a in funcs[func_name]["args"]:
+    print(f"   - {a}")
+
+def _search_func(first: str, second: str) -> str:
+  '''
+  Search the function list for the command, provide similar ones if not found exact match
+  '''
+  func_name = ""
+  for k,v in funcs.items():
+    if (v["first"] == first) & (v["second"] == second):
+      func_name = k
+      return func_name
+  
+  ### still didnt return:
+  return func_name
+
+def _search_func_by_id(func_id: int) -> str:
+  '''
+  Search the function list by id
+  '''
+  func_name = ""
+  for k,v in funcs.items():
+    if (v["id"] == func_id):
+      func_name = k
+      return func_name
+  
+  ### still didnt return:
+  return func_name
+
+
+def list_funcs() -> None:
+  print("\n")
+  for k,v in funcs.items():
+    print(f"[{v['id']}]: {k}")
+
+
+def _build_script(arg_dict: dict, func_id: int) -> None:
+  txt = f"""func_id = {func_id}; vars = {str(arg_dict)}"""
+  with open("scripts/console.py",'w') as f:
+    f.write(txt)
+
+def parse_command(command: str) -> None:
+  '''
+  First two arguments will be parsed into function name, rest will become arguments for the function
+  '''
+  cmds = command.split(" ")
+  n_args = cmds.__len__()
+  func_name = ""
+
+  if n_args < 2:
+    # check for invoking function by the function id
+    if (n_args == 1) & (cmds[0].isnumeric()):
+      func_name = _search_func_by_id(int(cmds[0]))
+    else:
+      print(f"\nWe definately need more arguments than {n_args}\n") 
+  else:
+    func_name = _search_func(cmds[0], cmds[1])
+
+  if func_name == "":
+    print(f"\n Cant find any function")
+  else:
+    print(f"\n Function: {func_name}")
+    _print_args(func_name)
+    arg_dict = _request_args(func_name)
+    _build_script(arg_dict, funcs[func_name]['id'])
+  
+
+
+def _request_args(func_name: str) -> dict:
+  '''
+  Request arguments per function. Input every single arguments by itself, returns dictionary of provided inputs
+  '''
+  args_dict = dict()
+  print("\n")
+  for a in funcs[func_name]["args"]:
+    args_dict[a] = input(f"{a}: ")
+    #if args_dict[a].isnumeric():
+    args_dict[a] = float(args_dict[a])
+  return args_dict
+
+def print_help(command: str) -> None:
+  '''
+  Print help for the command
+  '''
+  # remove the ? first
+  c = command.replace("?","")
+
