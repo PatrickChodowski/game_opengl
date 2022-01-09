817b47ab59badeef847432286602c2fb158c169f
patrick <chodowski.patrick@gmail.com>
2022-01-09 19:35:36 +0100

    making data driven blender

--- blender/dupa.py
+++ blender/dupa.py
@@ -1,19 +1,13 @@
 import bpy
 import numpy as np
 import math
+import importlib
 
-def make_grid_pos(a: float, b: float, axes: list) -> list:
-    # x,y,z -> 0,1,2
-    main = list()
-    if axes.__len__() == 2:
-        for y in np.arange(a, b, 1.0):
-            for z in np.arange(a, b, 1.0):
-                l = [0.0, 0.0, 0.0]
-                l[axes[0]] = y
-                l[axes[1]] = z
-                main.append(l)
-    return main
-            
+spec_grids = importlib.util.spec_from_file_location("grids", 
+"/home/patrick/Documents/projects/game_opengl/blender/grids.py")
+grids = importlib.util.module_from_spec(spec_grids)
+spec_grids.loader.exec_module(grids)
+ 
 def get_objects() -> list:
   objs = list()
   objects = bpy.context.editable_objects
@@ -32,16 +26,10 @@ def set_state(obj_list: list, state: bool) -> None:
         obj.select_set(state=state)
         print(f"{msg}selecting object {obj.name_full}")
             
-def activate(obj_index) -> None:
-    print(f"Activating object {objs[obj_index].name_full}")
-    bpy.context.view_layer.objects.active = objs[obj_index]
-    
-
 def move_collection(coll_name: str, x: float = None, y: float = None, z: float = None) -> None:
     set_state(bpy.data.collections[coll_name].objects, True)
     bpy.ops.transform.translate(value=[x,y,z], orient_type='GLOBAL')  
     set_state(bpy.data.collections[coll_name].objects, False)
-
 
 def make_new_coll(coll_name: str) -> str:
     collection = bpy.data.collections.new(coll_name)
@@ -57,28 +45,19 @@ def get_objs_by_names(obj_names: list) -> list:
             objs.append(obj)
     return objs
     
-    
 def get_object_location(obj_name: str) -> list:
     obj = bpy.data.objects[obj_name]
     loc = [obj.location.x, obj.location.y, obj.location.z]
     print(f"Location of {obj_name}: {loc}")
     return loc
 
-
-def copy_coll_objects(coll_name: str) -> str:
+def copy_collection(coll_name: str) -> str:
     new_coll_name = make_new_coll(coll_name)
     objs = bpy.data.collections[coll_name].objects
     for obj in objs:
         obj_copy = obj.copy()
         bpy.data.collections[new_coll_name].objects.link(obj_copy)
     return new_coll_name
-
-def make_coll_grid(coll_name: str, n: float, axes: list) -> list:
-    pos = make_grid_pos(1.0, n+1.0, axes)
-    for p in pos:
-        new_coll_name = copy_coll_objects(coll_name)
-        move_collection(new_coll_name, *p)
-    return pos
 
 def delete_collection(coll_name: str) -> None:
     collection = bpy.data.collections.get(coll_name)
@@ -101,7 +80,6 @@ def get_collection_rotation(coll_name: str):
         print(obj.rotation_euler)
         #print(obj.rotation_quaternion)
         
-    
 def rotate_collection(coll_name: str, x: int = None, y: int = None, z: int = None) -> None:
     set_state(bpy.data.collections[coll_name].objects, True)
     
@@ -119,22 +97,6 @@ def rotate_collection(coll_name: str, x: int = None, y: int = None, z: int = Non
         
     set_state(bpy.data.collections[coll_name].objects, False)
     
-def find_grid_center(pos: list) -> tuple:
-    xx = list()
-    yy = list()
-    zz = list()
-    adj_y = 2
-    adj_z = 2.9
-    for p in pos:
-        xx.append(p[0])
-        yy.append(p[1])
-        zz.append(p[2])
-    return (max(xx) - min(xx))/2, ((max(yy)+adj_y) - min(yy))/2, ((max(zz)+adj_z) - min(zz))/2 
-        
-    
-#bpy.data.collections.remove(bpy.data.collections.get('axe.004'))     
-
-
 def reset_camera():
     bpy.data.objects['Camera'].location.x = 0.0
     bpy.data.objects['Camera'].location.y = 0.0
@@ -143,35 +105,14 @@ def reset_camera():
     bpy.data.objects['Camera'].rotation_euler.y = 0.0
     bpy.data.objects['Camera'].rotation_euler.z = 0.0
 
-def align_camera_with_grid(x: float, y: float, z: float, align_on: str = "x"):
-    #  further on X axis, Y and Z the same
+def set_camera_pos(x: float, y: float, z: float) -> None:
     reset_camera()
-    if align_on == "x":
-        x = 2.0
-
-    if align_on == "y":
-        y = 2.0
-
-    if align_on == "z":
-        z = 2.0
-
     bpy.data.objects['Camera'].location.x = x
     bpy.data.objects['Camera'].location.y = y
     bpy.data.objects['Camera'].location.z = z
     bpy.data.objects['Camera'].rotation_euler.x = math.radians(90)
     bpy.data.objects['Camera'].rotation_euler.z = math.radians(+90)
     
-    
-def make_grid_rotations(coll_name: str, axes: list) -> None:
-    rots = [-105, -90, -45, -25, 0, 30, 45, 60, 75]
-    pos = make_coll_grid(coll_name, 3, axes)
-    colls = list_grid_collections(coll_name)
-    t = find_grid_center(pos)
-
-    for col, rot in zip(colls, rots):
-        rotate_collection(col, x=rot)
-    
-    align_camera_with_grid(*t) 
     
 def save(file_name: str, res_x: int, res_y: int) -> None:
     bpy.context.scene.render.image_settings.file_format = 'PNG'
@@ -186,3 +127,38 @@ def save(file_name: str, res_x: int, res_y: int) -> None:
     
 # make_grid_rotations("axe", axes=[1,2])
 # #save("axe")
+
+
+
+def resize_camera_view(x: float, y: float) -> None:
+  bpy.context.scene.render.resolution_x = x
+  bpy.context.scene.render.resolution_y = y
+
+
+
+def make_new_object(coll_name: str, rot: list, move: list) -> None:
+  """
+  Take collection, align it, rotate and move
+  """
+  new_coll_name = copy_collection(coll_name)
+  rotate_collection(new_coll_name, *rot)
+  move_collection(new_coll_name, *move)
+
+
+
+def make_objects_from_grid(coll_name: str, grid: grids.Grid) -> None:
+  """
+  Transforms collection based on the specified grid
+  """
+  print(f"Transforming {coll_name} by grid ID: {grid.id}")
+
+  for t in grid.transforms:
+    make_new_object(coll_name, t.rot, t.move)
+
+  set_camera_pos(*grid.camera_pos)
+
+
+  
+
+
+
--- blender/main_example.py
+++ blender/main_example.py
@@ -1,7 +1,15 @@
-import importlib.util
-spec = importlib.util.spec_from_file_location("dupa", 
-"/home/patrick/Documents/projects/game_opengl/blender/dupa.py")
-dupa = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(dupa)
 
-dupa.get_objects()
+import importlib.util
+spec_dupa = importlib.util.spec_from_file_location("dupa", 
+"/home/patrick/Documents/projects/game_opengl/blender/dupa.py")
+dupa = importlib.util.module_from_spec(spec_dupa)
+spec_dupa.loader.exec_module(dupa)\
+
+spec_grids = importlib.util.spec_from_file_location("grids", 
+"/home/patrick/Documents/projects/game_opengl/blender/grids.py")
+grids = importlib.util.module_from_spec(spec_grids)
+spec_grids.loader.exec_module(grids)
+
+#dupa.make_new_coll("axe")
+
+dupa.make_objects_from_grid("axe", grids.weapons_grid)
