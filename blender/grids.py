1016d1bbb841cf25d4f360a292be4e9410f0b961
patrick <chodowski.patrick@gmail.com>
2022-01-11 08:14:00 +0100

    added tag to texture data file

--- blender/grids.py
+++ blender/grids.py
@@ -39,7 +39,7 @@ class Grid:
     """
     p2 = list()
     for i in range(0,3):
-      print(p[i] + move[i])
+      #print(p[i] + move[i])
       p2.append(p[i] + move[i])
     return p2
 
@@ -54,43 +54,43 @@ axe_txt = Texture(8,"axe", 96, 96)
 
 # make transforms:
 w_trans = list()
-w_trans.append(Transform(id=0, rot=[-105, 0, 0], move=[0,1,1]))
-w_trans.append(Transform(id=1, rot=[-90,0, 0], move=[0,2,1]))
-w_trans.append(Transform(id=2, rot=[-45,0, 0], move=[0,3,1]))
+w_trans.append(Transform(id=0, rot=[-105, 0, 0], move=[0,1,1],    tag="side_face_down_105"))
+w_trans.append(Transform(id=1, rot=[-90,0, 0], move=[0,2,1],      tag="side_face_down_90"))
+w_trans.append(Transform(id=2, rot=[-45,0, 0], move=[0,3,1],      tag="side_face_level_45"))
 
-w_trans.append(Transform(id=3, rot=[-25,0, 0], move=[0,1,2]))
-w_trans.append(Transform(id=4, rot=[0,0, 0], move=[0,2,2]))
-w_trans.append(Transform(id=5, rot=[30,0, 0], move=[0,3,2]))
+w_trans.append(Transform(id=3, rot=[-25,0, 0], move=[0,1,2],      tag="side_face_level_25"))
+w_trans.append(Transform(id=4, rot=[0,0, 0], move=[0,2,2],        tag="side_original"))
+w_trans.append(Transform(id=5, rot=[30,0, 0], move=[0,3,2],       tag="side_face_up_30"))
 
-w_trans.append(Transform(id=6, rot=[45,0, 0], move=[0,1,3]))
-w_trans.append(Transform(id=7, rot=[60,0, 0], move=[0,2,3]))
-w_trans.append(Transform(id=8, rot=[75,0, 0], move=[0,3,3]))
+w_trans.append(Transform(id=6, rot=[45,0, 0], move=[0,1,3],       tag="side_face_up_45"))
+w_trans.append(Transform(id=7, rot=[60,0, 0], move=[0,2,3],       tag="side_face_behind_60"))
+w_trans.append(Transform(id=8, rot=[75,0, 0], move=[0,3,3],       tag="side_face_behind_75"))
 
 # facing "up"
-w_trans.append(Transform(id=9, rot=[-105, 0, 90], move=[0,1,4]))
-w_trans.append(Transform(id=10, rot=[-90,0, 90], move=[0,2,4]))
-w_trans.append(Transform(id=11, rot=[-45,0, 90], move=[0,3,4]))
+w_trans.append(Transform(id=9, rot=[-105, 0, 90], move=[0,1,4],   tag="up_face_down_105" ))
+w_trans.append(Transform(id=10, rot=[-90,0, 90], move=[0,2,4],    tag="up_face_down_90"  ))
+w_trans.append(Transform(id=11, rot=[-45,0, 90], move=[0,3,4],    tag="up_face_level_45" ))
 
-w_trans.append(Transform(id=12, rot=[-25,0, 90], move=[0,1,5]))
-w_trans.append(Transform(id=13, rot=[0,0, 90], move=[0,2,5]))
-w_trans.append(Transform(id=14, rot=[30,0, 90], move=[0,3,5]))
+w_trans.append(Transform(id=12, rot=[-25,0, 90], move=[0,1,5],    tag="up_face_level_25" ))
+w_trans.append(Transform(id=13, rot=[0,0, 90], move=[0,2,5],      tag="up_original"      ))
+w_trans.append(Transform(id=14, rot=[30,0, 90], move=[0,3,5],     tag="up_face_up_30"    ))
 
-w_trans.append(Transform(id=15, rot=[45,0, 90], move=[0,1,6]))
-w_trans.append(Transform(id=16, rot=[60,0, 90], move=[0,2,6]))
-w_trans.append(Transform(id=17, rot=[75,0, 90], move=[0,3,6]))
+w_trans.append(Transform(id=15, rot=[45,0, 90], move=[0,1,6],     tag="up_face_up_45"    ))
+w_trans.append(Transform(id=16, rot=[60,0, 90], move=[0,2,6],     tag="up_face_behind_60"))
+w_trans.append(Transform(id=17, rot=[75,0, 90], move=[0,3,6],     tag="up_face_behind_75"))
 
 # facing "down"
-w_trans.append(Transform(id=18, rot=[-105, 0, -90], move=[0,4,4]))
-w_trans.append(Transform(id=19, rot=[-90,0, -90], move=[0,5,4]))
-w_trans.append(Transform(id=20, rot=[-45,0, -90], move=[0,6,4]))
+w_trans.append(Transform(id=18, rot=[-105, 0, -90], move=[0,4,4], tag="down_face_down_105" ))
+w_trans.append(Transform(id=19, rot=[-90,0, -90], move=[0,5,4],   tag="down_face_down_90"  ))
+w_trans.append(Transform(id=20, rot=[-45,0, -90], move=[0,6,4],   tag="down_face_level_45" ))
 
-w_trans.append(Transform(id=21, rot=[-25,0, -90], move=[0,4,5]))
-w_trans.append(Transform(id=22, rot=[0,0, -90], move=[0,5,5]))
-w_trans.append(Transform(id=23, rot=[30,0, -90], move=[0,6,5]))
+w_trans.append(Transform(id=21, rot=[-25,0, -90], move=[0,4,5],   tag="down_face_level_25" ))
+w_trans.append(Transform(id=22, rot=[0,0, -90], move=[0,5,5],     tag="down_original"      ))
+w_trans.append(Transform(id=23, rot=[30,0, -90], move=[0,6,5],    tag="down_face_up_30"    ))
 
-w_trans.append(Transform(id=24, rot=[45,0, -90], move=[0,4,6]))
-w_trans.append(Transform(id=25, rot=[60,0, -90], move=[0,5,6]))
-w_trans.append(Transform(id=26, rot=[75,0, -90], move=[0,6,6]))
+w_trans.append(Transform(id=24, rot=[45,0, -90], move=[0,4,6],    tag="down_face_up_45"    ))
+w_trans.append(Transform(id=25, rot=[60,0, -90], move=[0,5,6],    tag="down_face_behind_60"))
+w_trans.append(Transform(id=26, rot=[75,0, -90], move=[0,6,6],    tag="down_face_behind_75"))
 
 # make grid:
 weapons_grid = Grid(id=0, 
