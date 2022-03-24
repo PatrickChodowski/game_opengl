79bb6db7972148bf13bdb4d4aff2ff1c2c152bbe
patrick <chodowski.patrick@gmail.com>
2022-03-24 12:51:29 +0100

    producing animations and angles

--- blender/main_anims.py
+++ blender/main_anims.py
@@ -10,13 +10,17 @@ spec_anims = importlib.util.spec_from_file_location("anims",
 anims = importlib.util.module_from_spec(spec_anims)
 spec_anims.loader.exec_module(anims)
 
-anims.clearout()
+MODEL_NAME = "warrior2"
+#anims.clearout()
 anims.create_camera()
 anims.create_center_empty()
 anims.make_camera_track_empty()
-camera_grid = anims.make_camera_loc_grid()
+camera_grid = anims.make_camera_loc_grid(4)
 
 for anim_name in anims.ANIM_LIST:
   anims.import_animation(anims.ANIM_PATH, anim_name)
+  anims.retarget(anim_name, "rig")
 
-anims.set_root_bone()
+  for camera_loc_name, camera_loc in camera_grid.items():
+    anims.set_camera_location(camera_loc['x'], camera_loc['y'], camera_loc['z'])
+    anims.render(MODEL_NAME, anim_name, camera_loc_name)
