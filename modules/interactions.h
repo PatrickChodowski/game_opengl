0814e1e970447fc031c91c1755849574522d913c
patrick <chodowski.patrick@chalhoub.com>
2022-05-10 17:41:42 +0400

    added interactions

--- modules/interactions.h
+++ modules/interactions.h
@@ -10,6 +10,41 @@
 
 namespace interactions
 {
+
+    struct StateMachine
+    {
+        int state_machine_id;
+        int current_state;
+        std::vector<int> states;
+    };
+
+    struct Encounter
+    {
+        int encounter_id;
+        std::vector<interactions::StateMachine> state_machines;
+
+    };
+
+
+    /*
+        Encounter:
+
+        0) doesnt know about the wold
+        1) knows about the wolf
+        2) seen the wolf
+        3a) killed the wolf
+        3b) not killed the wolf
+    */
+
+
+    // (internal) Gets index of current state
+    int _get_state_index(interactions::StateMachine &sm);
+
+
+    // Encounter will have multiple state machines?
+
+
+
     extern std::bitset<1000> history;
 
     // initialize interaction bitmap
